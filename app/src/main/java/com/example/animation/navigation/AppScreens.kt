16d259aa7f0961113b083dd1e0bd96909f0cc488
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2023-07-06 10:33:23 +0330

    home-screen (#2)

---
+++ app/src/main/java/com/example/animation/navigation/AppScreens.kt
@@ -0,0 +1,6 @@
+package com.example.animation.navigation
+
+enum class AppScreens(val value : String) {
+    HomeScreen("Home"),
+    BuiltInComposable("Built In Animation Composable")
+}
---
+++ app/src/main/java/com/example/animation/screens/home/HomeScreen.kt
@@ -0,0 +1,43 @@
+package com.example.animation.screens.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.animation.navigation.AppScreens
+import com.example.animation.components.AppTopBar
+import com.example.animation.components.GridCardItem
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Scaffold(topBar = {
+        AppTopBar(navController = navController)
+    }) {
+        LazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            columns = GridCells.Fixed(2),
+            content = {
+                items(AppScreens.values()) { item ->
+                    GridCardItem(item.value) {
+                        navController.navigate(item.name)
+                    }
+                }
+            })
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewHome() {
+    HomeScreen(navController = rememberNavController())
+}

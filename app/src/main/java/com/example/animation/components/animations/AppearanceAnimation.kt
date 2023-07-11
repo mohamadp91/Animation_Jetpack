b3cafe390970e38edf4364cd20bfe4d37c348eb6
mohamadp91 <mohamad7291m@gmail.com>
2023-07-11 14:14:37 +0330

    prepare the ui

--- app/src/main/java/com/example/animation/components/animations/AppearanceAnimation.kt
+++ app/src/main/java/com/example/animation/components/animations/AppearanceAnimation.kt
@@ -1,9 +1,72 @@
 package com.example.animation.components.animations
 
-import androidx.compose.material3.Text
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.center
+import androidx.compose.ui.unit.dp
+import com.example.animation.components.GridCardItem
+import com.example.animation.util.getScreenSize
 
 @Composable
 fun AppearanceAnimation() {
-    Text("Appearance Animation")
+    AppearanceGridLayout()
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AppearanceGridLayout() {
+    val appearanceAnimations = listOf(
+        "fade in" to fadeIn(),
+        "fade out" to fadeOut(),
+        "slide in horizontally" to slideInHorizontally(),
+        "slide out horizontally" to slideOutHorizontally(),
+        "slide in vertically" to slideInVertically(),
+        "slide out vertically" to slideOutVertically(),
+        "expand in" to expandIn(),
+        "shrink out" to shrinkOut(),
+        "expand horizontally" to expandHorizontally(),
+        "shrink horizontally" to shrinkHorizontally(),
+        "expand vertically" to shrinkVertically(),
+        "shrink vertically" to shrinkVertically(),
+        "shrink out" to shrinkOut(),
+        "scale in" to scaleIn(),
+        "scale out" to scaleOut(),
+        "slide in" to slideIn(initialOffset = { size -> size.center }),
+        "slide out" to slideOut(targetOffset = { size -> size.center }),
+    )
+
+    val density = LocalDensity.current
+    val (screenWidth, screenHeight) = getScreenSize(LocalConfiguration.current)
+    val cardWidth = (screenWidth / 2 - 24).dp
+    val cardHeight = (screenHeight / 4 - 24).dp
+
+    LazyVerticalGrid(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        columns = GridCells.Fixed(2),
+        content = {
+            items(appearanceAnimations) { item ->
+                GridCardItem(
+                    item.first,
+                    Pair(cardWidth, cardHeight),
+                ) {}
+            }
+        })
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Prev() {
+    AppearanceAnimation()
 }
--- app/src/main/java/com/example/animation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/animation/screens/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.animation.screens.home
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -8,33 +9,56 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.animation.components.AppTopBar
 import com.example.animation.components.GridCardItem
 import com.example.animation.navigation.AppScreens
+import com.example.animation.util.getScreenSize
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
     Scaffold(topBar = {
         AppTopBar(navController = navController)
     }) {
-        LazyVerticalGrid(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it),
-            columns = GridCells.Fixed(2),
-            content = {
-                items(AppScreens.values().filter { screen ->
-                    screen != AppScreens.HomeScreen
-                }) { item ->
-                    GridCardItem(item.value) {
-                        navController.navigate(item.name)
-                    }
-                }
-            })
+        CustomGridLayout(
+            paddingValues = it,
+            navController = navController
+        )
     }
+}
+
+@Composable
+fun CustomGridLayout(
+    paddingValues: PaddingValues,
+    navController: NavController
+) {
+    val (screenWidth, screenHeight) = getScreenSize(LocalConfiguration.current)
+
+    val cardWidth = (screenWidth / 2 - 24).dp
+    val cardHeight = (screenHeight / 3 - 24).dp
+
+    LazyVerticalGrid(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        columns = GridCells.Fixed(2),
+        content = {
+            items(AppScreens.values().filter { screen ->
+                screen != AppScreens.HomeScreen
+            }) { item ->
+                GridCardItem(
+                    item.value,
+                    Pair(cardWidth, cardHeight),
+                ) {
+                    navController.navigate(item.name)
+                }
+            }
+        })
 }
 
 
